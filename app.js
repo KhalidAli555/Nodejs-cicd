46eab49bbbcad9998cdc8a9fe1371394ae173934
khalid ali <mughalkhalid354@gmail.com>
2025-09-10 16:35:23 +0500

    updated main branch

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Welcome to IKS Cluster Pipeline!');
+  res.send('Welcome to SPS Internship!');
 });
 
 app.listen(port, () => {
